7aaad91503144de7aa42df2a2e57cb4f5e3937df
overflow33 <benamerihab25@gmail.com>
2020-02-19 13:27:48 +0100

    init commit

---
+++ src/test/java/com/ArchLog/cloudserver/CloudServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ArchLog.cloudserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServerApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
